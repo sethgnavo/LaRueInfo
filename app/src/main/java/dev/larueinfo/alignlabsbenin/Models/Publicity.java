9f324267ea8d323197dc21d0fdb9050ebae07eb9
GNAVO <gphares7@gmail.com>
2016-01-16 10:10:09 +0100

    Created class Publicity for ads management

---
+++ app/src/main/java/dev/larueinfo/alignlabsbenin/Models/Publicity.java
@@ -0,0 +1,54 @@
+package dev.larueinfo.alignlabsbenin.Models;
+
+/**
+ * @author Seth-Pharès Gnavo
+ */
+public class Publicity {
+    //image, contenu, date, nom de l'entreprise
+    private String adPictureUrl;
+    private String adTitle;
+    private String adAuthorName;
+    private long adTime;
+
+    public Publicity() {
+    }
+
+    public Publicity(String adPictureUrl, String adTitle, String adAuthorName, long adTime) {
+        this.adPictureUrl = adPictureUrl;
+        this.adTitle = adTitle;
+        this.adAuthorName = adAuthorName;
+        this.adTime = adTime;
+    }
+
+    public String getAdPictureUrl() {
+        return adPictureUrl;
+    }
+
+    public void setAdPictureUrl(String adPictureUrl) {
+        this.adPictureUrl = adPictureUrl;
+    }
+
+    public String getAdTitle() {
+        return adTitle;
+    }
+
+    public void setAdTitle(String adTitle) {
+        this.adTitle = adTitle;
+    }
+
+    public String getAdAuthorName() {
+        return adAuthorName;
+    }
+
+    public void setAdAuthorName(String adAuthorName) {
+        this.adAuthorName = adAuthorName;
+    }
+
+    public long getAdTime() {
+        return adTime;
+    }
+
+    public void setAdTime(long adTime) {
+        this.adTime = adTime;
+    }
+}
